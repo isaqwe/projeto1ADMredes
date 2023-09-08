fa819ead197a4d9fcdc67f544192afd74c95076c
isaqwe <isaquepontes47@gmail.com>
2023-09-08 16:06:12 -0300

    script vm2

---
+++ provisions/vm2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Atualizar a lista de pacotes
+sudo apt-get update
+
+# Instalar o servidor de banco de dados MySQL
+sudo apt-get install -y mysql-server
+
+# Configurar o servidor de banco de dados (opcional, dependendo das necessidades)
+
+# Iniciar o servidor de banco de dados
+sudo systemctl start mysql
+#!/bin/bash
+
+# Atualizar a lista de pacotes
+sudo apt-get update
+
+# Instalar o servidor de banco de dados MySQL
+sudo apt-get install -y mysql-server
+
+# Configurar o servidor de banco de dados (opcional, dependendo das necessidades)
+
+# Iniciar o servidor de banco de dados
+sudo systemctl start mysql
+
