907b8411cec9937bd8255a524ebb15abc0a10584
isaqwe <isaquepontes47@gmail.com>
2023-09-10 20:17:08 -0300

    update vm2

--- provision/vm2.sh
+++ provision/vm2.sh
@@ -23,3 +23,12 @@ sudo apt-get install -y mysql-server
 # Iniciar o servidor de banco de dados
 sudo systemctl start mysql
 
+#!/bin/bash
+
+# Configuração da interface de rede
+sudo ifconfig enp0s8 192.168.50.10 netmask 255.255.255.0 up
+
+# Adicionar rota padrão para a VM3 (Gateway)
+sudo ip route add default via 192.168.50.12 dev enp0s8
+
+# Outras configurações e comandos de instalação/configuração
