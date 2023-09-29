d44b1858be5639c6c6bd6700d385171ec2c6a2e5
isaqwe <isaquepontes47@gmail.com>
2023-09-29 10:02:58 -0300

    atualização

--- provision/vm1.sh
+++ provision/vm1.sh
@@ -13,8 +13,10 @@ sudo systemctl enable apache2
 sudo systemctl start apache2
 
 # Configuração da interface de rede
-sudo ifconfig enp0s8 192.168.50.10 netmask 255.255.255.0 up
+
+# sudo ifconfig enp0s8 192.168.50.10 netmask 255.255.255.0 up
 
 # Adicionar rota padrão para a VM3 (Gateway)
+route del default gw 10.0.2.2
 sudo ip route add default via 192.168.50.12 dev enp0s8
 
--- provision/vm2.sh
+++ provision/vm2.sh
@@ -21,9 +21,10 @@ sudo apt-get install -y mysql-server
 
 # Iniciar o servidor de banco de dados
 sudo systemctl start mysql
-
 # Configuração da interface de rede
-sudo ifconfig enp0s8 192.168.50.11 netmask 255.255.255.0 up
+
+# sudo ifconfig enp0s8 192.168.50.11 netmask 255.255.255.0 up
 
 # Adicionar rota padrão para a VM3 (Gateway)
+route del default gw 10.0.2.2
 sudo ip route add default via 192.168.50.12 dev enp0s8
